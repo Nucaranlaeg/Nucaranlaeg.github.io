014fde3beadde8a514a375fb3174265f0889c94f
Sam Benner <sbenner@nthds.com>
2020-02-26 11:05:28 -0600

    Add planBbot

--- KotH/GoGoL/bots.js
+++ KotH/GoGoL/bots.js
@@ -183,4 +183,119 @@ function planBot(grid, botId, lastMoves) {
     return best_cell;
 }
 
-let bots = [randomMovesBot, rgs_do_nothing, BestNowBot, planBot];
+// By Bob Genom, https://codegolf.stackexchange.com/questions/199658/game-of-game-of-life/200112#200112
+// version 1.0
+function planBbot(grid, botId, lastMoves) {
+    let t0=performance.now();
+    let best_cell = [-1, -1];
+    let max_score = -10000000;
+    let size = grid.length;
+    let possible_moves = new Set();
+    let dist = 1
+    for (let x = 0; x < size; x++) {
+        for (let y = 0; y < size; y++) {
+            if (grid[x][y] == botId) {
+                for (let dx = -dist; dx <= dist; dx++) {
+                    for (let dy = -dist; dy <= dist; dy++) {
+                        let x1 = (size + x + dx) % size;
+                        let y1 = (size + y + dy) % size;
+                        if ((grid[x1][y1] == 0) || (grid[x1][y1] == botId)) {
+                            possible_moves.add([x1, y1]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    let possible_move_array = Array.from(possible_moves);
+    if (possible_move_array.length == 0) {
+        return best_cell;
+    }
+
+    let new_grid = Array(size).fill(0).map(() => new Array(size).fill(0));
+    if (typeof this.iters == "undefined") {
+        this.iters=2;
+        this.sum=0;
+        this.count=1;
+        this.lookahead=function(old_grid, new_grid){
+            let iscore = 0;
+            let oscore = 0;
+            let size = old_grid.length;
+            let neighbours = [];
+            for (let x = 0; x < size; x++) {
+                for (let y = 0; y < size; y++) {
+                    neighbours.length = 0;
+                    for (let dx = -1; dx <= 1; dx++) {
+                        for (let dy = -1; dy <= 1; dy++) {
+                            if ((dx == 0) && (dy == 0)) {
+                                continue;
+                            }
+                            let cell = old_grid[(size + x + dx) % size][(size + y + dy) % size];
+                            if (cell != 0) {
+                                neighbours.push(cell);
+                            }
+                        }
+                    }
+                    let next=old_grid[x][y];
+                    if (neighbours.length < 2 || neighbours.length > 3) {
+                        next = 0;
+                    } else if (neighbours.length == 3 && old_grid[x][y] == 0) {
+                        if (neighbours[0] == neighbours[1] || neighbours[0] == neighbours[2]) {
+                            next = neighbours[0];
+                        } else if (neighbours[1] == neighbours[2]) {
+                            next = neighbours[1];
+                        } else {
+                            next = -1;
+                        }
+                    }
+                    new_grid[x][y] = next;
+                    if (next == botId) {
+                        iscore++;
+                    } else if (next != 0) {
+                        oscore++;
+                    }
+                }
+            }
+            return (iscore)-3*(oscore);
+        }
+    } else {
+        let avg=this.sum/this.count;
+        this.count++;
+        if (avg>49 && this.iters>1) {
+            this.iters--;
+        } else if (avg<49) {
+            this.iters++;
+        }
+    }
+    if (this.count>990) {
+        this.iters=1000-this.count;
+    }
+    if (this.count<200 && this.iters>6) {
+        this.iters=6;
+    }
+    for (cell of possible_move_array) {
+        let old_grid = grid.map(x => x.slice());
+        old_grid[cell[0]][cell[1]] = botId - old_grid[cell[0]][cell[1]];
+        let score = 0;
+        for (let i = 0; i < this.iters; i++) {
+            score += this.lookahead(old_grid, new_grid);
+            let tmp = new_grid;
+            new_grid = old_grid;
+            old_grid = tmp;
+        }
+
+        if (score >= max_score) {
+            max_score = score;
+            best_cell = cell;
+        }
+    }
+    let time=performance.now() - t0;
+    if (this.sum==0) {
+        this.sum=time;
+    } else {
+        this.sum+=time;
+    }
+    return best_cell;
+}
+
+let bots = [randomMovesBot, rgs_do_nothing, BestNowBot, planBot, planBbot];
