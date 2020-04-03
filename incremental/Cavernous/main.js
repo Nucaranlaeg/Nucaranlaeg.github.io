17642fbf5c11d25959f264b1adf02a5d3e743594
Sam Benner <sbenner@nthds.com>
2020-04-03 15:07:08 -0500

    Disable lag.  Disable jumping to active action

--- incremental/Cavernous/main.js
+++ incremental/Cavernous/main.js
@@ -774,7 +774,7 @@ function selectQueueAction(queue, action, percent){
 		percent += (complete / queues[queue][action][2].length) * 100;
 	}
 	node.querySelector(".progress").style.width = percent + "%";
-	queueNode.parentNode.scrollLeft = Math.max(action * 16 - (this.width / 2), 0);
+	// queueNode.parentNode.scrollLeft = Math.max(action * 16 - (this.width / 2), 0);
 }
 
 function scrollQueue(queue, action = null){
