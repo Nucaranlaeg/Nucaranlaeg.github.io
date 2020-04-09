dc1ca0091e9aaac22d42a1c6286181c3766666c5
Dimava <dimava2@ya.ru>
2020-04-09 15:52:20 +0300

    [=] and [G] keybinds

--- incremental/Cavernous/main.js
+++ incremental/Cavernous/main.js
@@ -558,6 +558,9 @@ let keyFunctions = {
 	"KeyB": () => {
 		toggleBankedTime();
 	},
+	"KeyG": () => {
+		toggleGrindMana();
+	},
 	"Tab": e => {
 		selectClone((selectedQueue[selectedQueue.length - 1] + 1) % clones.length);
 		e.stopPropagation();
@@ -587,6 +590,9 @@ let keyFunctions = {
 	},
 	"Digit3": () => {
 		addActionToQueue("N2");
+	},
+	"Equal" : () => {
+		addActionToQueue("=");
 	},
 };
 
