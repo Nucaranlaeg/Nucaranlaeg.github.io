7250217d160956fb31f152df5ddb505cd410f742
skillchip <121058167+skillchip@users.noreply.github.com>
2023-03-08 14:11:53 -0700

    Update and rename prestige to prestige.js
    Framework for Prestige system buttons and tracking. Needs corresponding changes to the save/load system in main

---
+++ incremental/CavernousII/dist/script/prestige.js
@@ -0,0 +1,39 @@
+"use strict";
+let prestigePoints = 0;
+class Prestige {
+  constructor () {
+    this.GameCompelte = 0;
+    this.Points = 0;
+    this.BonusClones = 0;
+    this.FasterStats = 0;
+    this.ManaScaling = 0;
+    this.BonusResc = 0;
+    this.BetterEquip = 0;
+    this.SoftCap = 0;
+    this.BonusZones = 0;
+  }
+}
+function BonusClones()
+{
+  
+}
+function FasterStats()
+{
+  
+}
+function ManaScaling()
+{
+  
+}
+function BonusResc()
+{
+  
+}
+function BetterEquip()
+{
+  
+}
+function SoftCap()
+{
+  
+}
